443d28a57856b5f377f430bec3715c21bab9731a
juzt3 <juzt3valentine@gmail.com>
2015-02-16 00:50:52 -0300

    First try to implement this function propely

--- src/main/java/CSVtoTPV/CSVtoTPV.java
+++ src/main/java/CSVtoTPV/CSVtoTPV.java
@@ -5,12 +5,23 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
+import de.taimos.gpsd4java.api.IObjectListener;
+import de.taimos.gpsd4java.types.ATTObject;
+import de.taimos.gpsd4java.types.DeviceObject;
+import de.taimos.gpsd4java.types.DevicesObject;
+import de.taimos.gpsd4java.types.IGPSObject;
+import de.taimos.gpsd4java.types.SKYObject;
 import de.taimos.gpsd4java.types.TPVObject;
+import de.taimos.gpsd4java.types.subframes.SUBFRAMEObject;
 
 public class CSVtoTPV {
 	
 	private TPVObject tpv;
+	
+	private final List<IObjectListener> listeners = new ArrayList<IObjectListener>(1);
 	
 	public CSVtoTPV (){
 		tpv = new TPVObject();
@@ -47,6 +58,28 @@ public class CSVtoTPV {
 	
 	public TPVObject getTPV(){
 		return tpv;
+	}
+	
+	public void addListener(final IObjectListener listener) {
+		this.listeners.add(listener);
+	}
+	
+	public void removeListener(final IObjectListener listener) {
+		this.listeners.remove(listener);
+	}
+	
+	void handle(final IGPSObject object) {
+		if (object instanceof TPVObject) {
+			for (final IObjectListener l : this.listeners) {
+				l.handleTPV((TPVObject) object);
+			}
+		} /*else {
+			// object was requested, so put it in the response object
+			synchronized (this.asyncWaitMutex) {
+				this.asnycResult = object;
+				this.asyncWaitMutex.notifyAll();
+			}
+		}*/
 	}
 
 }
